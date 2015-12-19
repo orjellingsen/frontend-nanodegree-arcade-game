e1337e66bf6299cb41c2f3ce2b7c4bd2ba68bfa3
orjellingsen <orjanell@gmail.com>
2015-12-20 00:35:45 +0100

    Added Player class and methods update(), render(), handleInput()

--- js/app.js
+++ js/app.js
@@ -24,7 +24,13 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+};
+
+Player.prototype.update = function(){};
+Player.prototype.render = function(){};
+Player.prototype.handleInput = function(){};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
