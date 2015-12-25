831d5570d0f0120794acce0b0ad5b465b79416ff
orjellingsen <orjanell@gmail.com>
2015-12-25 23:41:39 +0100

    Added all player and enemy functions. They are empty at the moment

--- js/app.js
+++ js/app.js
@@ -25,6 +25,18 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
+    this.sprite ='images/char-boy.png'
+};
+
+Player.update = function() {
+
+};
+
+Player.render = function() {
+
+};
+
+Player.handleInput = function() {
 
 };
 
@@ -35,7 +47,8 @@ Player.prototype.handleInput = function(){};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = Player;
 
 
 // This listens for key presses and sends the keys to your
