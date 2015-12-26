1e89b5ca6e80492331f08a8ca2124681ba53cbb3
orjellingsen <orjanell@gmail.com>
2015-12-26 12:50:04 +0100

    Added update function for enemies, making them move. If they reach the end of the screen, reset x coordinate

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x * 101;
+    this.x = this.x - 110;
+    this.y = y * 73;
 };
 
 // Update the enemy's position, required method for game
@@ -14,26 +17,25 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > 510) {
+        this.x = -110;
+    } else {
+        this.x += 3;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    startCol = 0; // Using the grid laid out in engine.js
-    startRow = 3;
-    this.x = startCol * 101;
-    this.y = startRow * 83;
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var Player = function(x, y) {
     this.sprite = 'images/char-boy.png';
-};
-
-Player.prototype.startPosition = function() {
-
+    this.x = x * 101;
+    this.y = y * 83;
 };
 
 // Update player's position
@@ -43,10 +45,6 @@ Player.prototype.update = function(dt) {
 
 // Draw the player on the screen
 Player.prototype.render = function() {
-    startCol = 2; // Using the grid laid out in engine.js
-    startRow = 5;
-    this.x = startCol * 101;
-    this.y = startRow * 83;
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -59,9 +57,9 @@ Player.prototype.handleInput = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
-var player = new Player();
-var enemy = new Enemy();
+var allEnemies = [new Enemy(0,1), new Enemy(0,2), new Enemy(0,3)];
+var player = new Player(2, 5);
+//var enemy = new Enemy();
 
 
 // This listens for key presses and sends the keys to your
