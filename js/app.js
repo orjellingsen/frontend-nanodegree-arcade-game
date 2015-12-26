d8b0c443e867a34ca35b22e631940bc915a6400d
orjellingsen <orjanell@gmail.com>
2015-12-26 17:08:47 +0100

    e

--- js/app.js
+++ js/app.js
@@ -7,23 +7,42 @@ var Enemy = function(x, y) {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = (x * 101) - 110; // Offset x-coordinate to make bug start outside screen
-    this.y = y * 73;
+    if (y === 1) {
+        this.y = y * 81;
+    } else if (y === 2) {
+        this.y = y * 81;
+    } else if (y === 3) {
+        this.y = y * 81;
+    }
     this.speed = Math.floor((Math.random() * 4) + 2); // Random number between 2-4 to determine enemy speed
+    this.collisionWidth = 40;
+    this.collisionHeight = 20;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function(dt, player) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x > 510) { // If enemy goes outside screen, reset to the left side and change its speed
         this.x = -110; 
-        this.speed = Math.floor((Math.random() * 4) + 1);
+        this.speed = Math.floor((Math.random() * 4) + 2);
     } else {
-        this.x += this.speed; // If enemy is still in the screen, set its movement speed
+        if (this.x === player.x && this.y === player.y) {
+            player.reset();
+        } else {
+            this.x += this.speed; // If enemy is still in the screen, set its movement speed
+            this.checkCollision(player);
+        }
     }
 };
+
+Enemy.prototype.checkCollision = function(player) {
+    if (Math.abs(player.x - this.x) < this.collisionWidth && Math.abs(player.y - this.y) < this.collisionHeight) {
+        player.reset();
+    }
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -36,19 +55,30 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function(x, y) {
     this.sprite = 'images/char-boy.png';
-    this.x = x * 101;
+    this.x = x * 101; // Starting position
     this.y = y * 81;
     this.speed = 2;
+    this.score = 0; // Player score starts at 0
 };
 
 // Update player's position
 Player.prototype.update = function(dt) {
-
+    if (this.y < 0) {
+        player.reset();
+        this.score += 1; // Add one to score and reset player when it reaches the water
+        console.log('Score: ' + this.score);
+    }
 };
 
 // Draw the player on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Reset player to original position
+Player.prototype.reset = function() {
+    this.x = 2 * 101;
+    this.y = 5 * 81;
 };
 
 // Handle the input to move the player
